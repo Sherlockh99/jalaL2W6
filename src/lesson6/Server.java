2e726bfe1ab2421724ac68f63f873a1ff126d38f
Sherlockh99 <sherlockh989@gmail.com>
2022-01-14 23:14:18 +0200

    Part 2

--- src/lesson6/Server.java
+++ src/lesson6/Server.java
@@ -1,6 +1,7 @@
 package lesson6;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
@@ -10,19 +11,25 @@ public class Server {
     private static Socket socket;
     private static final int PORT = 8189;
     private static Scanner sc;
+    private static PrintWriter out;
 
     public static void main(String[] args) {
         try (ServerSocket serverSocket = server = new ServerSocket(PORT)) {
             System.out.println("Server started!");
             socket = server.accept();
             System.out.println("Client connected!");
+
             sc = new Scanner(socket.getInputStream());
+            //out = new PrintWriter(socket.getOutputStream()); //для оптимизации используется буфер, поэтому ответы не будут приходить сразу
+            out = new PrintWriter(socket.getOutputStream(),true); //сразу отправляет
+
             while (true){
                 String str = sc.nextLine();
                 if (str.equals("/end")){
                     break;
                 }
                 System.out.println("Client: " + str);
+                out.println("ECHO: " + str);
             }
         }catch(IOException e){
             e.printStackTrace();
